e3987de8603079c433d1f3c2dd3e7e4e79e77267
enrloport <e.l.o.universidad@gmail.com>
2024-01-28 10:35:38 +0100

    updates in fashion_gpu run

--- runs/deepESN/deepESN_tanh_fashion_GPU.jl
+++ runs/deepESN/deepESN_tanh_fashion_GPU.jl
@@ -19,8 +19,8 @@ _params = Dict{Symbol,Any}(
     ,:classes       => [0,1,2,3,4,5,6,7,8,9]
     ,:beta          => 1.0e-8
     ,:initial_transient=>2
-    ,:train_length  => size(train_y)[1] -55000
-    ,:test_length   => size(test_y)[1] -9000
+    ,:train_length  => size(train_y)[1] #-55000
+    ,:test_length   => size(test_y)[1] #-9000
     ,:train_f       => __do_train_deepESN_mnist!
     ,:test_f        => __do_test_deepESN_mnist!
 )
@@ -29,68 +29,6 @@ _params = Dict{Symbol,Any}(
 px      = 28 # rand([14,20,25,28])
 sz      = (px,px)
 
-
-
-function do_batch_deepESN(_params_esn, _params,sd)
-    sz       = _params[:image_size]
-    im_sz    = sz[1]*sz[2] 
-    nodes    = _params_esn[:nodes] #im_sz
-    rhos     = _params_esn[:rho]
-    sigmas   = _params_esn[:sigma]
-    sgmds    = _params_esn[:sgmds]
-    densities= _params_esn[:density]
-    alphas   = _params_esn[:alpha]
-    r_scales = _params_esn[:R_scaling]
-
-    esn1 = ESN( 
-    #     R      = _params[:gpu] ? CuArray(new_R(nodes[1], density=densities[1], rho=rhos[1])) : new_R(nodes[1], density=densities[1], rho=rhos[1])
-    #    ,R_in   = _params[:gpu] ? CuArray(rand(Uniform(-sigmas[1],sigmas[1]), nodes[1], im_sz )) : rand(Uniform(-sigmas[1],sigmas[1]), nodes[1], im_sz )
-        R =  new_R(nodes[1], density=densities[1], rho=rhos[1], gpu = _params[:gpu])
-        ,R_in = new_R_in(nodes[1], im_sz, sigma=sigmas[1], gpu=_params[:gpu])
-        ,R_scaling = r_scales[1], alpha = alphas[1], rho = rhos[1], sigma = sigmas[1], sgmd = sgmds[1]
-    )
-    esn2 = [
-        ESN( 
-            #  R      = _params[:gpu] ? CuArray(new_R(nodes[i], density=densities[i], rho=rhos[i])) : new_R(nodes[i], density=densities[i], rho=rhos[i])
-            # ,R_in   = _params[:gpu] ? CuArray(rand(Uniform(-sigmas[i],sigmas[i]), nodes[i], nodes[i-1] )) : rand(Uniform(-sigmas[i],sigmas[i]), nodes[i], nodes[i-1] )
-            R =  new_R(nodes[i], density=densities[i], rho=rhos[i], gpu = _params[:gpu])
-            ,R_in = new_R_in(nodes[i], im_sz, sigma=sigmas[i], gpu=_params[:gpu])
-            ,R_scaling = r_scales[i]
-            ,alpha  = alphas[i]
-            ,rho    = rhos[i]
-            ,sigma  = sigmas[i]
-            ,sgmd   = sgmds[i]
-        ) for i in 2:_params[:num_esns]
-    ]
-
-    tms = @elapsed begin
-        deepE = deepESN(
-            esns=[esn1, esn2...]
-            ,beta=_params[:beta] 
-            ,train_function = _params[:train_f]
-            ,test_function = _params[:test_f]
-            )
-        tm_train = @elapsed begin
-            deepE.train_function(deepE,_params)
-        end
-        tm_test = @elapsed begin
-            deepE.test_function(deepE,_params)
-        end
-    end
- 
-    to_log = Dict(
-        "Total time"  => tms
-        ,"Train time"=> tm_train
-        ,"Test time"=> tm_test
-       , "Error"    => deepE.error
-    )
-    if _params[:wb] 
-        Wandb.log(_params[:lg], to_log )
-    else
-        display(to_log)
-    end
-    return deepE
-end
 
 
 for _ in 1:repit
