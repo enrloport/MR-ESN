b133e3148cb0201eec00f678fa2165a652dd68ab
enrloport <e.l.o.universidad@gmail.com>
2024-01-28 10:34:49 +0100

    updates in update function

--- files/update_functions/update.jl
+++ files/update_functions/update.jl
@@ -1,8 +1,8 @@
 
 function update(esn,u, f)
-    # println(typeof(esn.R_in))
-    # println(typeof(u))
-    # println(typeof(esn.R))
-    # println(typeof(esn.x))
+    # println("R_in", typeof(esn.R_in) , " size: ", size(esn.R_in))
+    # println("u", typeof(u) , " size: ", size(u))
+    # println("R", typeof(esn.R) , " size: ", size(esn.R_in))
+    # println("x", typeof(esn.x) , " size: ", size(esn.x))
     esn.x[:] = (1-esn.alpha).*esn.x .+ esn.alpha.*esn.sgmd.( esn.F_in(f,u) .+ esn.R*esn.x)
 end
