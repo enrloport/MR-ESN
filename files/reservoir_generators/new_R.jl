5176a0cc2d1fe433736081dac3f679513ad3cfb0
enrloport <e.l.o.universidad@gmail.com>
2024-01-29 07:57:35 +0100

    update in new_R function

--- files/reservoir_generators/new_R.jl
+++ files/reservoir_generators/new_R.jl
@@ -1,15 +1,15 @@
 
 
 function new_R( R_size::Int=50; R_scaling::Float64=1.0, rho::Float64=1.0, density=1.0, distribution=Uniform, gpu=false, bounds=(0,0))
-    up,low = -R_scaling, R_scaling
+    low,up = -R_scaling, R_scaling
     if bounds[1] + bounds[2] != 0
-        up,low = bounds[1], bounds[2]
+        low,up = bounds[1], bounds[2]
     end
     if density != 1.0
-        W = sprand(R_size, R_size, density, x-> rand(distribution(up, low), x) )
+        W = sprand(R_size, R_size, density, x-> rand(distribution(low,up), x) )
         W = Array(W)
     else
-        W = rand( distribution( up, low ) , R_size, R_size )
+        W = rand( distribution( low,up ) , R_size, R_size )
     end
     set_spectral_radius!( W , rho)
 
